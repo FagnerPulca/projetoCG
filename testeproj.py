d983ee7dcfd809aa12bb18655bd24a256081d380
Ihago Santos <ihagog12@gmail.com>
2018-07-25 15:19:52 -0300

    Iluminação

--- testeproj.py
+++ testeproj.py
@@ -11,6 +11,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
+
 class Cube(object):
     def __init__(self, posicao):
         self.posicao = posicao
@@ -73,8 +74,9 @@ class Cube(object):
         for edge in edges:
             for vertex in edge:
                 glTexCoord2f(texture_vertices[i][0], texture_vertices[i][1])
+                
                 glVertex3fv(verticies[vertex])
-
+                
                 i += 1
                 if (i > 3):
                     i = 0
@@ -101,6 +103,10 @@ class Ground(object):
             (0,1)
             )
         i = 0
+
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, 1)
+        
         glBegin(GL_QUADS)
         for vertice in chao:
             glTexCoord2f(texture_vertices[i][0], texture_vertices[i][1])
@@ -122,9 +128,11 @@ class Map():
             for j in range(len(mapa[i])):
                 if (mapa[i][j] == 1):
                     cubo = Cube((i*2,0,j*2))
+                    
                     self.cubos.append(cubo)
                 if (mapa[i][j] == 0):
                     ground = Ground((i*2,0,j*2))
+                    
                     self.ground.append(ground)
 
     def desenhar(self):
@@ -133,14 +141,16 @@ class Map():
         for ground in self.ground:
             ground.desenhar()
         
-def iluminacao():
+def iluminacao(camera_x,camera_y,camera_z):
     
     glShadeModel(GL_SMOOTH)
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_LIGHTING)
     
-    lightZeroPosition = [10., 10, 6., 1.]
-    lightZeroColor = [0.5, 0.5, 0.5, 1.0]
+##    lightZeroPosition = [10., 10, 6., 1.]
+    lightZeroPosition = [camera_x,camera_y,camera_z, 1.]
+
+    lightZeroColor = [0.5, 0.5, 0.5, 1]
     lightEspecular = [2., 4., 10., 1.]
     
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
@@ -153,9 +163,9 @@ def iluminacao():
     
 
 #Função para carregar a textura
-def loadTexture():
+def loadTexture(textura):
 
-        textureSurface = pygame.image.load('textura_parede.jpeg') #carrega imagem da textura
+        textureSurface = pygame.image.load(textura) #carrega imagem da textura
         textureData = pygame.image.tostring(textureSurface,"RGBA",1)
         width = textureSurface.get_width()
         height = textureSurface.get_height()
@@ -171,8 +181,10 @@ def loadTexture():
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
+
         return texid
 
+    
 
 def main():
     pygame.init()
@@ -209,14 +221,16 @@ def main():
     glEnable(GL_DEPTH_TEST)
 
     #carrega Textura
-    loadTexture()
+    loadTexture('textura_parede.jpeg')
 
     #toca trilha sonora
     trilha_sonora.play()
 
 ##    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA)
-    iluminacao()
     
+    #iluminação
+##    iluminacao()
+
     while True:
             
         for event in pygame.event.get():
@@ -240,7 +254,7 @@ def main():
             camera_y = f[3][1]
             camera_z = f[3][2]
 
-            
+##            iluminacao(camera_x,camera_y,camera_z)
                         
             #print camera_x,",",camera_z
             
@@ -250,7 +264,11 @@ def main():
                 #print mapa.mapa[0][3]
                 x += lx * speed 
                 z += lz * speed 
-                #print camera_x,",",camera_z
+                print camera_x,",",camera_z
+
+                
+                
+                
                 
             if event.key == pygame.K_DOWN:
                 global lx, lz, x, z, speed
@@ -258,7 +276,10 @@ def main():
                 x -= lx * speed 
                 z -= lz * speed 
                 print camera_x,",",camera_z
+
+
                 
+                               
             if event.key == pygame.K_LEFT:
                 global lx, lz, angle
                 #Rotaciona a linha de visão em a esquerda
@@ -266,6 +287,8 @@ def main():
                 lx = math.sin(angle)
                 lz = math.cos(angle)
                 print camera_x,",",camera_z
+
+                
                 
             if event.key == pygame.K_RIGHT:
                 global angle, lx, lz
@@ -275,11 +298,13 @@ def main():
                 lz = math.cos(angle)
                 print camera_x,",",camera_z
 
+                
+            iluminacao(camera_x,camera_y,camera_z + 10)        
         # Reset transformations
         glLoadIdentity()
 	# Set the camera
         gluLookAt(x, 0.0, z,  x+lx, 0.0, z+lz,  0.0, -1.0,  0.0)
-            
+        
         pygame.display.flip()
         pygame.time.wait(10)
 
